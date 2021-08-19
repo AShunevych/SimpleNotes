47782e793683288f484b94c3cf12c090d2e10bd0
AShunevich <48925983+AShunevich@users.noreply.github.com>
2021-08-19 09:41:16 +0300

    - code update

--- app/src/main/java/com/ashunevich/simplenotes/NoteRepository.kt
+++ app/src/main/java/com/ashunevich/simplenotes/NoteRepository.kt
@@ -3,21 +3,19 @@ package com.ashunevich.simplenotes
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 
+@WorkerThread
 class NoteRepository(private val noteDao: NoteDao) {
 
     val allNotes: LiveData<List<NoteEntity>> = noteDao.getAll()
 
-    @WorkerThread
     fun insert(noteEntity: NoteEntity) {
         noteDao.insert(noteEntity)
     }
 
-    @Suppress("RedundantSuspendModifier")
     fun delete(noteEntity: NoteEntity){
         noteDao.delete(noteEntity)
     }
 
-    @Suppress("RedundantSuspendModifier")
     fun update(noteEntity: NoteEntity){
         noteDao.update(noteEntity)
     }
