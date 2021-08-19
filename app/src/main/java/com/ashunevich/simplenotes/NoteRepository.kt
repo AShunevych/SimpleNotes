02df5e495537a41dce7d0fed2e1113c3fcf350d6
AShunevich <48925983+AShunevich@users.noreply.github.com>
2021-08-19 09:41:00 +0300

    - code update

--- app/src/main/java/com/ashunevich/simplenotes/NoteRepository.kt
+++ app/src/main/java/com/ashunevich/simplenotes/NoteRepository.kt
@@ -7,21 +7,18 @@ class NoteRepository(private val noteDao: NoteDao) {
 
     val allNotes: LiveData<List<NoteEntity>> = noteDao.getAll()
 
-    @Suppress("RedundantSuspendModifier")
     @WorkerThread
-    suspend fun insert(noteEntity: NoteEntity) {
+    fun insert(noteEntity: NoteEntity) {
         noteDao.insert(noteEntity)
     }
 
     @Suppress("RedundantSuspendModifier")
-    @WorkerThread
-    suspend fun delete(noteEntity: NoteEntity){
+    fun delete(noteEntity: NoteEntity){
         noteDao.delete(noteEntity)
     }
 
     @Suppress("RedundantSuspendModifier")
-    @WorkerThread
-    suspend fun update(noteEntity: NoteEntity){
+    fun update(noteEntity: NoteEntity){
         noteDao.update(noteEntity)
     }
 
